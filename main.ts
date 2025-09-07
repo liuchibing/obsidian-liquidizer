37d34f7803ff3fca983c2037fca315d9ce6d8298
Liu Chibing <liuchibing@outlook.com>
2025-09-07 21:13:11 +0800

    first code

--- main.ts
+++ main.ts
@@ -1,89 +1,175 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { liquid } from "liquidHelpers";
+import {
+	App,
+	getFrontMatterInfo,
+	MarkdownView,
+	Notice,
+	parseYaml,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface LiquidizerPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: LiquidizerPluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class LiquidizerPlugin extends Plugin {
+	settings: LiquidizerPluginSettings;
+
+	// originalOnRender: any = null;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		const exportLiquidRenderedDocument = (checking: boolean) => {
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView && markdownView.file) {
+					if (checking) {
+						// This command can be run
+						return true;
 					}
+					const filePath = markdownView.file.path.replace(
+						".md",
+						"_rendered.md"
+					);
+					this.app.vault
+						.read(markdownView.file)
+						.then((content) => {
+							// get frontmatter info
+							const frontmatterInfo = getFrontMatterInfo(content);
+							if (!frontmatterInfo.exists) {
+								new Notice(
+									"Add `liquidize: true` to the frontmatter to enable rendering."
+								);
+								return Promise.reject(
+									new Error("No frontmatter found")
+								);
+							}
+							// parse frontmatter
+							const frontmatter = parseYaml(frontmatterInfo.frontmatter);
+							if (!frontmatter.liquidize) {
+								new Notice(
+									"Add `liquidize: true` to the frontmatter to enable rendering."
+								);
+								return Promise.reject(
+									new Error(
+										"`liquidize: true` not found in frontmatter"
+									)
+								);
+							}
+							// render content with liquid
+							return liquid.parseAndRender(content, { ...frontmatter });
+						})
+						.then((rendered) => {
+							// if file exists, overwrite it, otherwise create a new file
+							this.app.vault.adapter.write(filePath, rendered);
+							new Notice("Exported rendered document!");
+						})
+						.catch((error) => {
+							new Notice(
+								"Error rendering document: " + error.message
+							);
+						});
 
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+				return false;
 			}
+
+		// Command: Use liquid to render the current document and export it to a new document
+		this.addCommand({
+			id: "export-liquid-rendered-document",
+			name: "Export Liquid Rendered Document",
+			checkCallback: exportLiquidRenderedDocument,
 		});
+
+		this.addRibbonIcon("file-output", "Export Liquid Rendered Document", (evt: MouseEvent) => {
+			// Use the same logic as the command
+			exportLiquidRenderedDocument(false);
+		});
+
+		// console.log(MarkdownPreviewView);
+		// console.log(MarkdownPreviewView.prototype.onRender);
+		// this.originalOnRender = MarkdownPreviewView.prototype.onRender;
+
+		// const localOriginalOnRender = MarkdownPreviewView.prototype.onRender;
+		// MarkdownPreviewView.prototype.onRender = function (this: MarkdownPreviewView) {
+		// 	const proxyThis = new Proxy(this, {
+		// 		get(target, prop, receiver) {
+		// 			if (prop === "text") {
+		// 				try {
+		// 					return liquid.parseAndRenderSync(
+		// 						target.text,
+		// 						{ ...target.frontmatter }
+		// 					);
+		// 				} catch (error) {
+		// 					console.error("Error rendering liquid template:", error);
+		// 				}
+		// 			}
+		// 			return Reflect.get(target, prop, receiver);
+		// 		}
+		// 	});
+		// 	localOriginalOnRender.apply(proxyThis, arguments);
+		// }
+
+		// Provide Live Preview of Liquid Templates
+		// this.registerMarkdownPostProcessor(async (element, context) => {
+		// 	// Check if the frontmatter has a specific flag to enable liquid processing
+		// 	if (!context.frontmatter.liquidize) {
+		// 		return;
+		// 	}
+		// 	const sectionInfo = context.getSectionInfo(element);
+		// 	try {
+		// 		const rendered = await liquid.parseAndRender(
+		// 			element.innerHTML,
+		// 			{ ...context.frontmatter }
+		// 		);
+		// 		element.innerHTML = rendered;
+		// 	} catch (error) {
+		// 		new Notice("Error rendering liquid template: " + error);
+		// 	}
+		// }, Number.MIN_SAFE_INTEGER);
+
+		// when frontmatter changes, re-render the preview
+		// this.registerEvent(
+		// 	this.app.vault.on("modify", (file) => {
+		// 		const markdownView =
+		// 			this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (
+		// 			markdownView &&
+		// 			markdownView.file &&
+		// 			markdownView.file.path === file.path &&
+		// 			markdownView.currentMode instanceof MarkdownPreviewView
+		// 		) {
+		// 			markdownView.previewMode.rerender(true);
+		// 		}
+		// 	})
+		// );
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new LiquidizerPluginSettingTab(this.app, this));
 	}
 
 	onunload() {
-
+		// if (this.originalOnRender) {
+		// 	MarkdownPreviewView.prototype.onRender = this.originalOnRender;
+		// }
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +177,30 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class LiquidizerPluginSettingTab extends PluginSettingTab {
+	plugin: LiquidizerPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LiquidizerPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
